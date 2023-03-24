5c75be8581941932fc01ebe7d805493b1e6883aa
fengchi <fengchi@iflytek.com>
2023-03-24 21:44:25 +0800

    feat: 测试添加ts之后重新构建

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }: {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[池峰测试pull预览]</p>
+        <p>[池峰测试pull预览 + ts]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
